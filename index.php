accaa3706f6c3b37f160e815f243f549cb1a2a61
bellomellomeow <bellomellomeow37@gmail.com>
2023-11-27 16:03:32 +0800

    index.php change

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ session_start();
 include('DBConnection.php');
 $pdo = DBConnection::connectToDB();
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if (isset($_POST['uname'])&& isset($_POST['password'])){
+//if (isset($_POST['uname'])&& isset($_POST['password'])){
+if (isset($_POST['uname'])&& isset($_POST['password']) && isset($_POST['role'])){
     function validate($data){
         $data= trim($data);
         $data = stripslashes($data);
@@ -17,6 +17,9 @@ if (isset($_POST['uname'])&& isset($_POST['password'])){
     //giving a variable that the user typw in the box
     $uname = validate($_POST['uname']);
     $pass = validate($_POST['password']);
+    //$hashPassword = password_hash($pass, PASSWORD_BCRYPT);
+    $role = validate($_POST['role']);
+    
     //if the username box is empty
      if (empty($uname)) {
         header("Location: index.php?error=User name is required");
@@ -24,42 +27,62 @@ if (isset($_POST['uname'])&& isset($_POST['password'])){
      }elseif (empty($pass)){
          header("Location: index.php?error=Password is required");
          exit();
-     //}else {
      }else {
-       
+     
     //$sql = "SELECT employee.Employee_ID, employee.Name, sensitive_info.Password
-        //FROM employee
-       // INNER JOIN sensitive_info ON employee.Employee_ID = sensitive_info.Sensitive_Info_ID
+        ////FROM employee
+        //INNER JOIN sensitive_info ON employee.Employee_ID = sensitive_info.Sensitive_Info_ID
         //WHERE BINARY employee.Name = :uname AND BINARY sensitive_info.Password = :pass";
-         $sql = "SELECT employee.Employee_ID, employee.Name, sensitive_info.Password, employee.Profile_Pic
+         $sql = "SELECT employee.Employee_ID, employee.Name, sensitive_info.Password, employee.Profile_Pic, employee.Role_ID, role.Role_Name 
                 FROM employee
                 INNER JOIN sensitive_info ON employee.Employee_ID = sensitive_info.Sensitive_Info_ID
+                INNER JOIN role ON employee.Role_ID = role.Role_ID
                 WHERE BINARY employee.Name = :uname";
-        $query = $pdo->prepare($sql);
-        $query->bindParam(':uname', $uname, PDO::PARAM_STR);
-        //$query->bindParam(':pass', $pass, PDO::PARAM_STR);
-        $query->execute();
-        $data = $query->fetch(PDO::FETCH_ASSOC);
-       
-        //if ($data) {
-            //$_SESSION['Name'] = $data['Name'];
-            //$_SESSION['Employee_ID'] = $data['Employee_ID'];
-            //header("Location: home.php");
-            //exit();
+               
+         
+//          $sql = "SELECT employee.Employee_ID, employee.Name, sensitive_info.Password,role.Role_Name 
+//                  FROM employee
+//                  INNER JOIN sensitive_info ON employee.Employee_ID = sensitive_info.Sensitive_Info_ID
+//                  INNER JOIN role ON employee.Role_ID = role.Role_ID
+//                  WHERE BINARY employee.Name = :uname AND BINARY sensitive_info.Password = :pass AND role.Role_ID = :role";
+         
+         $query = $pdo->prepare($sql);
+         $query->bindParam(':uname', $uname, PDO::PARAM_STR);
+//        $query->bindParam(':pass', $hashPassword, PDO::PARAM_STR);
+//         $query->bindParam(':role', $role, PDO::PARAM_STR);
+         $query->execute();
+         $data = $query->fetch(PDO::FETCH_ASSOC);
+         
+         //var_dump($data['Role_Name']);
+         //var_dump($role);
+//        var_dump($data);
+    
         
-        if ($data && password_verify($pass, $data['Password'])) {
+         //if ($data && password_verify($pass, $data['Password'])&&strcasecmp($data['role.Role_Name'], $role) == 0) {
+         if ($data && password_verify($pass, $data['Password'])&& $data['Role_Name'] ===  $role) {
             
              //Password is correct
-            $_SESSION['Name'] = $data['Name'];
-            $_SESSION['Employee_ID'] = $data['Employee_ID'];
+             $_SESSION['Name'] = $data['Name'];
+ //            $_SESSION['Role_Name'] = $data['Role_Name'];
+             $_SESSION['Role_Name'] = $data['Role_Name'];
+             $_SESSION['Employee_ID'] = $data['Employee_ID'];
+             //$_SESSION['Role_ID'] = $data['Role_ID']; 
+//             var_dump($_SESSION);           
             //$_SESSION['Profile_pic'] = $data['Profile_pic'];
-            header("Location: home.php");
-            exit();
+             header("Location: home.php");
+             exit();
         
         } else {
+            //if ($data && strcasecmp($data['Role_Name'], $role) != 0) {
+            if ( $data['Role_ID'] !=  $role) {
+                header("Location: index.php?error=Incorrect Role");
+                
+                exit();
+            }
+            
             header("Location: index.php?error=Incorrect User name or password");
             exit();
-        }
+         }
 }
 }
 ?>
@@ -93,8 +116,29 @@ if (isset($_POST['uname'])&& isset($_POST['password'])){
 			<input type="text" name="uname" placeholder="Username" style="display: block; border:2px solid #ccc;width:95%;padding:10px;margin:10px auto;"><br>
 			<label style="color:#888;font-size:18px;padding:10px">Password</label>
 			<input type="password" name="password" placeholder="Password" style="display: block; border:2px solid #ccc;width:95%;padding:10px;margin:10px auto;"><br>
+			<!--  <input type="text" name="role" placeholder="name" style="display: block; border:2px solid #ccc;width:95%;padding:10px;margin:10px auto;"><br>-->
 			<button type="submit" style="float: right; background: #555;padding:10px 15px; color:#fff;border-radius:5px;margin-right:10px;">Login</button>
-		    
+		     <div class="control-group">
+					<label for="Role">role</label>
+					<div class="controls">
+
+						<select name="role" required>
+						<?php
+                       
+                       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $selectRoleSQL = "SELECT * FROM role";
+                        $query = $pdo->prepare($selectRoleSQL, array(
+                            PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL
+                        ));
+                        $query->execute();
+                        $data = $query->fetchAll();
+                        foreach ($data as $row) {
+                            echo "<option value=" . $row['Role_Name'] . ">" . $row['Role_Name'] . "</option>";
+                        }
+                        ?>
+            			</select>
+					</div>
+				</div>
 		<!-- 
 		<label>User Name</label>
 		<input type="text" name="uname" placeholder="Username"><br>
