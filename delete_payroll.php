04c0cf5e5060ef285b9aed2889b055ba1dee3320
halfbraceo <jovytanys@gmail.com>
2023-11-29 08:57:47 +0800

    delete_payroll.php

---
+++ delete_payroll.php
@@ -0,0 +1,20 @@
+<?php
+include "dbConnection.php";
+$id = 0;
+if ( !empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+    }
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
+        
+        // delete data
+        $pdo = DBConnection::connectToDB();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM payroll  WHERE Payroll_ID = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        DBConnection::disconnect();
+        header("Location: index.php");
+    }
+?>
