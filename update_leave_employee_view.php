cec3f223019eb1e8b54d3d61fcbb47738e386e30
hhchen22 <hanghong2005@gmail.com>
2023-11-30 00:13:44 +0800

    RetrieveLeaveEmployee.php

--- update_leave_employee_view.php
+++ update_leave_employee_view.php
@@ -1,74 +1,85 @@
 <?php
 require 'DBConnection.php';
 
-$id = 0;
-
-if (!empty($_GET['leave_id'])) {
-    $id = $_REQUEST['leave_id'];
-}
-
+// Check if the request is for updating leave details
 if (!empty($_POST)) {
-    // Keep track of post values
-    $id = $_POST['leave_id'];
-    $start_date = $_POST['start_date'];
-    $end_date = $_POST['end_date'];
-    $reason = $_POST['reason'];
+    // Update leave details
+    $leave_id = $_POST['leave_id'];
+    $from_date = $_POST['from_date'];
+    $until_date = $_POST['until_date'];
+    $notes = $_POST['notes'];
 
-    // Update data
+    // Update data in the database
     $pdo = DBConnection::connectToDB();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "UPDATE leave_records SET start_date = ?, end_date = ?, reason = ? WHERE leave_id = ?";
+    $sql = "UPDATE leave SET From_Date = ?, Until_Date = ?, Notes = ? WHERE leave_ID = ?";
     $q = $pdo->prepare($sql);
-    $q->execute(array($start_date, $end_date, $reason, $id));
+    $q->execute(array($from_date, $until_date, $notes, $leave_id));
     DBConnection::disconnect();
-    header("Location: index.php"); // Assuming you have an index page for leave records
+
+    // Redirect to employee view page
+    header("Location: employee_view.php"); // Assuming you have an employee view page
+    exit;
 }
 
-// Retrieve leave details for the selected employee
+// Check if the request is for retrieving leave details
 if (isset($_GET["leave_id"])) {
-    $id = $_GET["leave_id"];
+    $leave_id = $_GET["leave_id"];
 
     // Retrieve leave details from the database
     $pdo = DBConnection::connectToDB();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT * FROM leave_records WHERE leave_id = ?";
+    $sql = "SELECT * FROM leave WHERE leave_ID = ?";
     $q = $pdo->prepare($sql);
-    $q->execute(array($id));
+    $q->execute(array($leave_id));
     $row = $q->fetch(PDO::FETCH_ASSOC);
+}
 
-    // Display the form with current leave details
+// Display the form with current leave details
+if (isset($row)) {
+    // Leave details found
     ?>
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="js/bootstrap.min.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <div class="span10 offset1">
-                <div class="row">
-                    <h3>Update Leave</h3>
-                </div>
-                <form class="form-horizontal" action="update_leave.php" method="post">
-                    <input type="hidden" name="leave_id" value="<?php echo $row['leave_id']; ?>">
-                    Start Date: <input type="date" name="start_date" value="<?php echo $row['start_date']; ?>"><br>
-                    End Date: <input type="date" name="end_date" value="<?php echo $row['end_date']; ?>"><br>
-                    Reason: <textarea name="reason"><?php echo $row['reason']; ?></textarea><br>
-                    <div class="form-actions">
-                        <button type="submit" class="btn btn-primary">Update</button>
-                        <a class="btn" href="index.php">Cancel</a>
-                    </div>
-                </form>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="bg-light">
+    <?php include('SideNav.php')?>
+    <div class="container-fluid mt-4">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-5">
+                <li class="breadcrumb-item"><a href="Home.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">View Employees</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Register Employee</li>
+            </ol>
+        </nav>
+
+        <div class="container" id="form">
+            <div class="text-center">
+                <h3>Update Leave</h3>
             </div>
-        </div> <!-- /container -->
-    </body>
-    </html>
-    <?php
+            <form action="update_leave_employee_view.php?leave_id=<?php echo $leave_id; ?>" method="post">
+                <div class="mb-3">
+                    <label for="from_date" class="form-label">From Date</label>
+                    <input type="date" class="form-control" id="from_date" name="from_date" value="<?php echo $row['From_Date']; ?>" required>
+                </div>
 
-    // Close the database connection
-    DBConnection::disconnect();
-}
-?>
+                <div class="mb-3">
+                    <label for="until_date" class="form-label">Until Date</label>
+                    <input type="date" class="form-control" id="until_date" name="until_date" value="<?php echo $row['Until_Date']; ?>" required>
+                </div>
 
+                <div class="mb-3">
+                    <label for="notes" class="form-label">Notes</label>
+                    <textarea class="form-control" id="notes" name="notes" rows="3"><?php echo $row['Notes']; ?></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success">Update</button>
+                    <a class="btn btn
+ 
