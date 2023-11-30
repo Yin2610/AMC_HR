aa35546163ba20fbc8f61cab43fc96fc74f062f9
hhchen22 <hanghong2005@gmail.com>
2023-11-30 14:30:23 +0800

    DeleteEmployee.php

--- DeleteEmployee.php
+++ DeleteEmployee.php
@@ -24,30 +24,44 @@
  
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
- 
-<body>
-    <div class="container">
-     
-                <div class="span10 offset1">
-                    <div class="row">
-                        <h3>Delete employee</h3>
+
+<body class="bg-light">
+    <?php include('SideNav.php')?>
+    <div class="container-fluid mt-4">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-5">
+                <li class="breadcrumb-item"><a href="Home.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">View Employees</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Delete Employee</li>
+            </ol>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="text-center">
+                        <h3>Delete Employee</h3>
                     </div>
-                     
+
                     <form class="form-horizontal" action="delete.php" method="post">
-                      <input type="text" name="id" value="<?php echo $id;?>"/>
-                      <p class="alert alert-error">Are you sure to delete ?</p>
-                      <div class="form-actions">
-                          <button type="submit" class="btn btn-danger">Yes</button>
-                          <a class="btn" href="index.php">No</a>
+                        <input type="text" name="id" value="<?php echo $id; ?>"/>
+                        <p class="alert alert-danger">Are you sure you want to delete this employee?</p>
+                        <div class="form-actions">
+                            <button type="submit" class="btn btn-danger">Yes</button>
+                            <a class="btn btn-secondary" href="index.php">No</a>
                         </div>
                     </form>
                 </div>
-                 
-    </div> <!-- /container -->
-  </body>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
+ 
