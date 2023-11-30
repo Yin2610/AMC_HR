e550acb7e3a5dad2d489ccdbaa1d26d791314cca
Mabel <yma.meemee@gmail.com>
2023-11-30 17:29:14 +0800

    Remove the line $id=$_SESSION['id]

--- SideNav.php
+++ SideNav.php
@@ -4,9 +4,6 @@ if(!isset($_SESSION['Employee_ID']) || $_SESSION['Employee_ID'] == '') {
     echo "<script>alert('Please login first.')</script>";
     header("Location: index.php");
 }
-else {
-    $id = $_SESSION['Employee_ID'];
-}
 ?>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
